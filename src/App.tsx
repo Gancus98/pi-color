c776f3962c1d0b2f7a6d2100c5d488530e92bd37
DawidGarnczarek <gancus98@gmail.com>
2021-10-16 14:56:37 +0200

    Add base navBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import CanvasArea from './components/CanvasArea';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <CanvasArea />
     </div>
   );
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Styled from './NavBar.styles';
+
+const NavBar: React.FC = () => {
+  return (
+    <Styled.NavWrapper>
+      <p>NAVBAR</p>
+    </Styled.NavWrapper>
+  );
+};
+
+export default NavBar;
